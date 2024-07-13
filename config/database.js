ee8e78b510d9db7115b5cc055d06410c27ff6336
itrcvivek <itrcvivek@gmail.com>
2024-07-14 01:53:35 +0530

    server.js

--- config/database.js
+++ config/database.js
@@ -1,16 +1,13 @@
 const mongoose = require("mongoose");
 
-const connectDatabse = ( ) =>{
+const connectDatabase = () => {
+    mongoose.connect(process.env.DB_URI)
+    .then((data) => {
+        console.log(`MongoDB connected with server: ${data.connection.host}`);
+    }).catch((error) => {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    });
+};
 
-    mongoose.connect(process.env.DB_URI,{ useNewUrlParser: true, 
-        useUnifiedTopology: true,authSource:'admin' })
-        .then((data) => {
-        
-            console.log(`mongodb connected with sever: ${data. connection. host}`);
-        }).catch((err)=>{
-console.log(err)
-        })
-      
-}
-
-module.exports=connectDatabse;
+module.exports = connectDatabase;
--- server.js
+++ server.js
@@ -1,53 +1,39 @@
 const app = require("./app");
-
 const dotenv = require("dotenv");
-const connectDatabse = require("./config/database");
+const connectDatabase = require("./config/database");
 const cloudinary = require("cloudinary");
-const http = require("http")
 
-
-process.on("uncaughtException",(err)=>{
-    console.log('Error: ${err.message}');
-    console.log('server was shutdown ');
-        process.exit(1);
- 
+// Handle uncaught exceptions
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log('Server is shutting down due to an uncaught exception');
+    process.exit(1);
 });
 
-dotenv.config({ path: "config/config.env" });
+// Load environment variables from config.env file
+dotenv.config({ path: "./config.env" });
 
-
-
-
+// Cloudinary configuration
 cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
-}); 
-
-connectDatabse();
-
-
-const server =app.listen(process.env.PORT, () => {
-    console.log(`server is working on http://localhost:${process.env.PORT}`)
 });
 
+// Connect to the database
+connectDatabase();
 
+// Start the server
+const server = app.listen(process.env.PORT || 6000, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT || 6000}`);
+});
 
-// unhandled promise rejaction
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log('Server is shutting down due to an unhandled promise rejection');
 
-process.on("unhandledRejection",(err)=>{
-    console.log('Error: ${err.message}');
-    console.log('server was shutdown ');
-
-    server.close(()=>{
+    server.close(() => {
         process.exit(1);
     });
 });
-
-
-
-
-
-
-
-
